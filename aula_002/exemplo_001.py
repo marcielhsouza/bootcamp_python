e2d92e3c164e03f4d080cbf662c6587fd341f02d
Marciel <marciel.hsouza@gmail.com>
2026-05-11 19:02:56 -0300

    exemplo_001 - Adição

---
+++ aula_002/exemplo_001.py
@@ -0,0 +1,10 @@
+#Inteiros (int)
+
+#Métodos e operações:
+#        + (adição)
+print(f"Adição {10} + {10} = {10+10}")    
+
+#        - (subtração)
+#        * (multiplicação)
+#        // (divisão inteira)
+#        % (módulo - resto da divisão)
