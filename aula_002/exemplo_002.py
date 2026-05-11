e127bade4cae7a5ec8e079c5636ad9e67fa205e6
Marciel <marciel.hsouza@gmail.com>
2026-05-11 19:17:27 -0300

    exemplo_002 - Adição

---
+++ aula_002/exemplo_002.py
@@ -0,0 +1,10 @@
+#Números de Ponto Flutuante (float)
+
+#Métodos e operações:
+# + (adição)
+print(f"Adição: {4.9} + {4.9} = {4.9+4.9}")   
+
+# - (subtração)
+# * (multiplicação)
+# / (divisão)
+# ** (potenciação)
