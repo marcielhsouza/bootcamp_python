e94535c5b4ac905a90ce206c21dc57b5003c6c87
Marciel <marciel.hsouza@gmail.com>
2026-05-12 12:14:22 -0300

    exemplo_003 - .lower()

--- aula_002/exemplo_003.py
+++ aula_002/exemplo_003.py
@@ -7,6 +7,10 @@ print(f"Sem .upper() = {nome}")
 print(f"Com .upper() = {nome.upper()}")
 
 #  .lower() (converte para minúsculas)
+nome = "MARCIEL"
+print(f"Sem .lower()  = {nome}")
+print(f"Com .lower() = {nome.lower() }")
+
 #  .strip() (remove espaços em branco no início e no final)
 #  .split(sep) (divide a string em uma lista, utilizando sep como delimitador)
 #  + (concatenação de strings)
