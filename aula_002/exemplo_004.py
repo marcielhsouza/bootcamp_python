028ade46d7ef36401a5c912c8843ec757d07d27a
Marciel <marciel.hsouza@gmail.com>
2026-05-12 13:45:15 -0300

    exemplo_004 - Booleano operador AND

---
+++ aula_002/exemplo_004.py
@@ -0,0 +1,11 @@
+# Booleanos (bool)
+
+# Operações lógicas:
+# and (E lógico)
+print(f"Booleano TRUE com o operador AND '5==5 and 5==5': {5==5 and 5==5}")
+print(f"Booleano FALSE com o operador AND '5==5 and 5==5': {5==4 and 5==5}")
+
+# or (OU lógico)
+# not (NÃO lógico)
+# == (igualdade)
+# != (diferença)
