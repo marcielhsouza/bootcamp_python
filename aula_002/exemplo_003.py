3ff37c30b05ff6215db676f9da634437d475a0b1
Marciel <marciel.hsouza@gmail.com>
2026-05-12 11:19:39 -0300

    exemplo_003 - .upper()

---
+++ aula_002/exemplo_003.py
@@ -0,0 +1,12 @@
+# Strings (str)
+
+#    Métodos e operações:
+# .upper() (converte para maiúsculas)
+nome = "marciel"
+print(f"Sem .upper() = {nome}")
+print(f"Com .upper() = {nome.upper()}")
+
+#  .lower() (converte para minúsculas)
+#  .strip() (remove espaços em branco no início e no final)
+#  .split(sep) (divide a string em uma lista, utilizando sep como delimitador)
+#  + (concatenação de strings)
