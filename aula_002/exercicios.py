6162027365ad2c8e92709f08e2ffe9a77d529ec8
Marciel <marciel.hsouza@gmail.com>
2026-05-13 09:03:10 -0300

    exercicio - 010 adicionado

--- aula_002/exercicios.py
+++ aula_002/exercicios.py
@@ -69,7 +69,6 @@ area_circ = 3.14 * (raio**2)
 print(f"A área to circulo é de : {area_circ}")
 
 # Strings (str)
-
 # 011 - Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
 # 012 - Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 # 013 - Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
