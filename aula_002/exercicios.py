090a127d599134d2d8f484f0ae0ca32f50a828d5
Marciel <marciel.hsouza@gmail.com>
2026-05-15 13:07:47 -0300

    exercicio - 021 adicionado

--- aula_002/exercicios.py
+++ aula_002/exercicios.py
@@ -135,7 +135,35 @@
 
 # 020 - Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.]
 
-valor_bool1 = 10
-valor_bool2 = 10
-result_dif = valor_bool1 != valor_bool2
-print(f"O resultado da expressão booleana '!=' entre dois valores é: {result_dif}")
+# valor_bool1 = 10
+# valor_bool2 = 10
+# result_dif = valor_bool1 != valor_bool2
+# print(f"O resultado da expressão booleana '!=' entre dois valores é: {result_dif}")
+
+# 021 - Conversor de Temperatura
+# Escreva um programa que converta a temperatura de Celsius para Fahrenheit. O programa deve solicitar ao usuário a temperatura em Celsius e, utilizando try-except, 
+# garantir que a entrada seja numérica, tratando qualquer ValueError. 
+# Imprima o resultado em Fahrenheit ou uma mensagem de erro se a entrada não for válida.
+
+try:
+   temp_celcius = float(input("Digite a temperatura (Celsius): "))
+   fahrenheit = (temp_celcius * 1.8) + 32
+   print(f"A temperatura digitada (Celsius) é de {fahrenheit} em Fahrenheit")
+except ValueError as e:
+    print("Digite um número válido.")
+
+# 022 - Verificador de Palíndromo 
+# Crie um programa que verifica se uma palavra ou frase é um palíndromo (lê-se igualmente de trás para frente, desconsiderando espaços e pontuações). 
+# Utilize try-except para garantir que a entrada seja uma string. Dica: Utilize a função isinstance() para verificar o tipo da entrada.
+
+# 023 - Calculadora Simples 
+# Desenvolva uma calculadora simples que aceite duas entradas numéricas e um operador (+, -, *, /) do usuário. 
+# Use try-except para lidar com divisões por zero e entradas não numéricas. Utilize if-elif-else para realizar a operação matemática baseada no operador fornecido. 
+# Imprima o resultado ou uma mensagem de erro apropriada.
+
+# 024 - Classificador de Números 
+# Escreva um programa que solicite ao usuário para digitar um número. Utilize try-except para assegurar que a entrada seja numérica e utilize if-elif-else para 
+# classificar o número como "positivo", "negativo" ou "zero". Adicionalmente, identifique se o número é "par" ou "ímpar".
+
+# 025 - Conversão de Tipo com Validação 
+# Crie um script que solicite ao usuário uma lista de números separados por vírgula. O programa deve converter a string de entrada em uma lista de números inteiros. Utilize try-except para tratar a conversão de cada número e validar que cada elemento da lista convertida é um inteiro. Se a conversão falhar ou um elemento não for um inteiro, imprima uma mensagem de erro. Se a conversão for bem-sucedida para todos os elementos, imprima a lista de inteiros.
