7a0f3e02b088d605ca5e71e50d10bc594df397f7
Marciel <marciel.hsouza@gmail.com>
2026-05-21 09:40:01 -0300

    exercicio - 025 adicionado

--- aula_002/exercicios.py
+++ aula_002/exercicios.py
@@ -197,24 +197,33 @@
 # Escreva um programa que solicite ao usuário para digitar um número. Utilize try-except para assegurar que a entrada seja numérica e utilize if-elif-else para 
 # classificar o número como "positivo", "negativo" ou "zero". Adicionalmente, identifique se o número é "par" ou "ímpar".
 
-try:
-    num = int(input("Digite um número: "))
-    if num < 0:
-        print(f"Negativo")
-    elif num > 0:
-        print(f"Positivo")
-    else:
-        print(f"Zero")
-    if num%2==0:
-        print(f"Par")
-    else:
-        print(f"Impar")
-except ValueError as e:
-    print("Digite apenas números inteiros")
+# try:
+#     num = int(input("Digite um número: "))
+#     if num < 0:
+#         print(f"Negativo")
+#     elif num > 0:
+#         print(f"Positivo")
+#     else:
+#         print(f"Zero")
+#     if num%2==0:
+#         print(f"Par")
+#     else:
+#         print(f"Impar")
+# except ValueError as e:
+#     print("Digite apenas números inteiros")
     
-    
-
-
-
 # 025 - Conversão de Tipo com Validação 
-# Crie um script que solicite ao usuário uma lista de números separados por vírgula. O programa deve converter a string de entrada em uma lista de números inteiros. Utilize try-except para tratar a conversão de cada número e validar que cada elemento da lista convertida é um inteiro. Se a conversão falhar ou um elemento não for um inteiro, imprima uma mensagem de erro. Se a conversão for bem-sucedida para todos os elementos, imprima a lista de inteiros.
+# Crie um script que solicite ao usuário uma lista de números separados por vírgula. 
+# O programa deve converter a string de entrada em uma lista de números inteiros. 
+# Utilize try-except para tratar a conversão de cada número e validar que cada elemento da lista convertida é um inteiro. 
+# Se a conversão falhar ou um elemento não for um inteiro, imprima uma mensagem de erro.Se a conversão for bem-sucedida para todos os elementos,imprima a lista de inteiros.
+
+lista_num = input("Digite uma lista de números separados por virgula: ")
+lista = lista_num.split(",")
+lista_num_int = []
+try:
+    for num in lista:
+        lista_num_int.append(int(num.strip()))
+    print(lista_num_int)
+except ValueError as e:
+    print("Erro: certifique-se de que todos os elementos são números inteiros válidos.")
