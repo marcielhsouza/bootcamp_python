97d60e6112a1a7e66fce01809f6712206b2cf3b3
Marciel <marciel.hsouza@gmail.com>
2026-05-30 17:40:05 -0300

    exercicio_013 - adicionado

--- aula_004/exercicios.py
+++ aula_004/exercicios.py
@@ -121,28 +121,25 @@
 
 # Objetivo: Dados dois dicionários, fundi-los em um único dicionário.
 
-dicionario1 = {"a": 1, "b": 2}
-dicionario2 = {"c": 3, "d": 4}
-dicionario_fundido = {**dicionario1, **dicionario2}
-print(dicionario_fundido)
+# dicionario1 = {"a": 1, "b": 2}
+# dicionario2 = {"c": 3, "d": 4}
+# dicionario_fundido = {**dicionario1, **dicionario2}
+# print(dicionario_fundido)
 
 # 13) Filtragem de Dados em Dicionário
 
 # Objetivo: Dado um dicionário de estoque de produtos, filtrar aqueles com quantidade maior que 0.
 
-# estoque = {"Teclado": 10, "Mouse": 0, "Monitor": 3, "CPU": 0}
-
-# estoque_positivo = {produto: quantidade for produto, quantidade in estoque.items() if quantidade > 0}
-
-# print(estoque_positivo)
+estoque = {"Teclado": 10, "Mouse": 0, "Monitor": 3, "CPU": 0}
+estoque_positivo = {produto:quantidade for produto, quantidade in estoque.items() if quantidade > 0}
+print(estoque_positivo)
 
 # 14) Extração de Chaves e Valores
 
 # Objetivo: Dado um dicionário, criar listas separadas para suas chaves e valores.
 
 # dicionario = {"a": 1, "b": 2, "c": 3}
-# chaves = list(dicionario.keys())
-# valores = list(dicionario.values())
+
 
 # print("Chaves:", chaves)
 # print("Valores:", valores)
